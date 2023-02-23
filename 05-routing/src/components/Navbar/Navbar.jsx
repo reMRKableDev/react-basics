52620d5da77bede3c8ea2c1c1a91e2f88acd12f2
LloydC <lloydchambrier@gmail.com>
2023-02-23 10:43:05 +0000

    🎨 Update NavLink component according to v6.4 standard

--- 05-routing/src/components/Navbar/Navbar.jsx
+++ 05-routing/src/components/Navbar/Navbar.jsx
@@ -4,14 +4,18 @@ import { Link, NavLink } from "react-router-dom";
 /* <Link> --> Attach clickable links to different pages. */
 /* <NavLink> --> special version of <Link> that adds styling attributes. */
 const Navbar = () => {
+  let activeStyle = { color: "red" };
+
   return (
     <nav className="navbar is-spaced is-light">
       <div className="navbar-menu is-active" id="navbarBasicExample">
         <div className="navbar-end">
           <NavLink
             className="navbar-item"
-            activeStyle={{ color: "red" }}
             to="/"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
           >
             Home
           </NavLink>
