d3e0baf78291cadd9fd4be7bc6613acd12979d8d
LloydC <lloydchambrier@gmail.com>
2023-02-23 11:08:54 +0000

    🩹(GifApp.js): Add eslint ignore comment

--- 04-lifecycle-methods/src/components/Root/App.js
+++ 04-lifecycle-methods/src/components/Root/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./App.css";
 
 /* Simple Example of Lifecycle Methods */
-import CounterApp from "../CounterApp/CounterRoot/CounterApp";
+// import CounterApp from "../CounterApp/CounterRoot/CounterApp";
 
 /* A More Complex Example - combines class components & functional components  */
-import GifApp from "../GiphyApp/GifRoot/GifApp";
+// import GifApp from "../GiphyApp/GifRoot/GifApp";
 
 import GifAppHooksOnly from "../GiphyAppFunctionalOnly/GifRoot/GifApp";
 
